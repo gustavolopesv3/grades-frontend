a3b1b3b177c7f6e93f4cc6097beb8eb48c7ccb44
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-10-26 18:47:04 -0500

    clear

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
 
 export default class App extends Component {
   render() {
-    return <ProjetoBase />;
+    return <p>Hello hooks</p>;
   }
 }
